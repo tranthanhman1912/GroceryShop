dd643bbcafd035ec3d54cad50af19c007e89b2ac
Trần Thanh Mẫn <62742886+tranthanhman1912@users.noreply.github.com>
2021-07-15 19:05:21 +0700

    chua reponsive

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// Toggle menu account
+$(".myAccount").click(function () { 
+  $(".dropdown-menu-account").toggle();
+});
+
+$(".Currency").click(function () { 
+  $(".dropdown-menu-currency").toggle();
+});
+
+$(".Language").click(function () { 
+  $(".dropdown-menu-language").toggle();
+});
+
+
 // Initialize Swiper 
 var swiper = new Swiper(".mySwiper", {});
+var swiperSlider = new Swiper(".mySwiper-logo-slider", {
+  watchSlidesProgress: true,
+  watchSlidesVisibility: true,
+  slidesPerView: 7,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
