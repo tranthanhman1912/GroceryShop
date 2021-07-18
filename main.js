7ee5bf3c3cfc37247533e10c7d5587e0e150f0c2
Trần Thanh Mẫn <62742886+tranthanhman1912@users.noreply.github.com>
2021-07-18 17:27:45 +0700

    fix swipper

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Toggle menu account
+// Toggle top navbar 
 $(".myAccount").click(function () { 
   $(".dropdown-menu-account").toggle();
 });
@@ -13,7 +13,74 @@ $(".Language").click(function () {
 
 
 // Initialize Swiper 
-var swiper = new Swiper(".mySwiper", {});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  breakpoints: {
+    425: {
+      slidesPerView: 2,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    }
+  },
+  
+});
+
+var swiperSlideShow = new Swiper(".mySwiperSlideShow", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//swiper specials
+var swiperSpecials = new Swiper(".swipSpecials", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  breakpoints: {
+    425: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 920px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    }
+  }
+});
+
+//swiper before footer
 var swiperSlider = new Swiper(".mySwiper-logo-slider", {
   watchSlidesProgress: true,
   watchSlidesVisibility: true,
@@ -22,10 +89,15 @@ var swiperSlider = new Swiper(".mySwiper-logo-slider", {
     delay: 2500,
     disableOnInteraction: false,
   },
+  
 });
 
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+})
+
+$('.carousel').carousel({
+  interval: 5000,
 })
 
