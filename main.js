bc86486d8b8393ce697971e33efc38b20c45d732
Trần Thanh Mẫn <62742886+tranthanhman1912@users.noreply.github.com>
2021-07-20 10:25:15 +0700

    90%

--- main.js
+++ main.js
@@ -42,14 +42,17 @@ var swiper = new Swiper(".mySwiper", {
   },
   
 });
-
+//  Swiper SlideShow (right side)
 var swiperSlideShow = new Swiper(".mySwiperSlideShow", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 4000,
+    
+  },
 });
-
 //swiper specials
 var swiperSpecials = new Swiper(".swipSpecials", {
   slidesPerView: 4,
@@ -77,7 +80,8 @@ var swiperSpecials = new Swiper(".swipSpecials", {
       slidesPerView: 4,
       spaceBetween: 10
     }
-  }
+  },
+  
 });
 
 //swiper before footer
@@ -89,10 +93,81 @@ var swiperSlider = new Swiper(".mySwiper-logo-slider", {
     delay: 2500,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    425: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 10
+    },
+    // when window width is >= 920px
+    992: {
+      slidesPerView: 7,
+      spaceBetween: 10
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 7,
+      spaceBetween: 10
+    }
+  },
   
 });
 
+//swiper blog
+var swiperBlog = new Swiper(".mySwiperBlog", {
+  slidesPerView: 2,
+  breakpoints: {
+    425: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 920px
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    }
+  },
+  
+});
+//end blog
 
+//end before footer
+
+//footer (reponsive 576px)
+$( "#showListf-extras" ).click(function() {
+  $( "#listf-extras" ).toggle( "slow");
+});
+$( "#showListf-info" ).click(function() {
+  $( "#listf-info" ).toggle( "slow");
+});
+$( "#showListf-account" ).click(function() {
+  $( "#listf-account" ).toggle( "slow");
+});
+$( "#showListf-contact" ).click(function() {
+  $( "#listf-contact" ).toggle( "slow");
+});
+//end footer
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
@@ -100,4 +175,15 @@ $(function () {
 $('.carousel').carousel({
   interval: 5000,
 })
+
+
+
+// responsive 576px nav
+$( ".toogle-category" ).click(function() {
+  $( ".navbar-responsive" ).show( "slow");
+});
+$( "#close" ).click(function() {
+  $( ".navbar-responsive" ).hide( "slow");
+});
+
 
